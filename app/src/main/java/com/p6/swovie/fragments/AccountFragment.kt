11b2a4779c5bd405ef7b949ab209a026b879246d
Mai <maimaih0708@gmail.com>
2021-04-16 11:07:45 +0200

    Made Reset password and log out possible, start on Login screen

--- app/src/main/java/com/p6/swovie/fragments/AccountFragment.kt
+++ app/src/main/java/com/p6/swovie/fragments/AccountFragment.kt
@@ -1,7 +1,9 @@
 package com.p6.swovie.fragments
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +12,10 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.p6.swovie.LoginActivity
 import com.p6.swovie.R
 
 class AccountFragment : Fragment(), View.OnClickListener {
@@ -19,6 +25,8 @@ class AccountFragment : Fragment(), View.OnClickListener {
     private lateinit var buttonResetSwipes: Button
     private lateinit var buttonResetPassword: Button
     private lateinit var buttonLogout: Button
+
+    var auth: FirebaseAuth = Firebase.auth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,18 +61,23 @@ class AccountFragment : Fragment(), View.OnClickListener {
 
     override fun onClick(view: View?) { // All OnClick buttons, with strings depending on button
         when (view) {
-            buttonResetSwipes -> alert(getString(R.string.resetswipes), getString(R.string.alertswipes))
-            buttonResetPassword -> alert(getString(R.string.resetpassword), getString(R.string.alertpassword))
-            buttonLogout -> alert(getString(R.string.logout), getString(R.string.alertlogout))
+            buttonResetSwipes -> alert(getString(R.string.resetswipes), getString(R.string.alertswipes), buttonResetSwipes)
+            buttonResetPassword -> alert(getString(R.string.resetpassword), getString(R.string.alertpassword), buttonResetPassword)
+            buttonLogout -> alert(getString(R.string.logout), getString(R.string.alertlogout), buttonLogout)
         }
     }
 
-    private fun alert(title: String, message: String){ //Making an alert dialog
+    private fun alert(title: String, message: String, button: Button){ //Making an alert dialog
         val builder = AlertDialog.Builder(activity)
         builder.setTitle(title)
         builder.setMessage(message)
         builder.setPositiveButton(R.string.alertyes) { dialogInterface, which ->
-            Toast.makeText(activity, "clicked yes", Toast.LENGTH_LONG).show()
+            when (button) {
+                buttonLogout -> { logOut() }
+                buttonResetPassword -> { resetPassword() }
+                buttonResetSwipes -> { Toast.makeText(activity, "Reset all swipes clicked yes", Toast.LENGTH_LONG).show() }
+                else -> { Toast.makeText(activity, "Error occurred", Toast.LENGTH_LONG).show() }
+            }
         }
         builder.setNeutralButton(R.string.alertcancel){dialogInterface , which ->
         }
@@ -74,5 +87,28 @@ class AccountFragment : Fragment(), View.OnClickListener {
         alertDialog.show()
     }
 
+    private fun logOut() {
+        auth.signOut()
+        val intent = Intent(activity, LoginActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+
+    private fun resetPassword(){
+        val user = Firebase.auth.currentUser
+        user?.let {
+            for (profile in it.providerData) {
+                val email = profile.email
+                Firebase.auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+
+                        }
+                        Log.d("AccountFragment", "Email sent.")
+                    }
+            }
+        }
+
+    }
 
 }
