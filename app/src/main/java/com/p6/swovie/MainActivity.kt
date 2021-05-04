36e9c68d5d5ec5a605f3dccf9010e7ca5c090355
tehsnah <jimmerjc@gmail.com>
2021-05-04 09:49:02 +0200

    mainactivity small fixes

--- app/src/main/java/com/p6/swovie/MainActivity.kt
+++ app/src/main/java/com/p6/swovie/MainActivity.kt
@@ -4,20 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.p6.swovie.fragments.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG: String = "MainActivity"
 
     private val movieFragment = MovieFragment()
-    private val matchFragment = CreateGroupFragment()
-    private val secondMatchFragment = MatchFragment()
+    private val createGroupFragment = CreateGroupFragment()
+    private val matchFragment = MatchFragment()
     private val searchFragment = SearchFragment()
     private val accountFragment = AccountFragment()
     private var isInGroup: Boolean = false
@@ -35,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation.isClickable = false
 
         if(auth.currentUser != null) {
-            db.collection("rooms").whereArrayContains("users", auth.currentUser.uid).get()
+            db.collection("groups").whereArrayContains("users", auth.currentUser.uid).get()
                 .addOnSuccessListener { document ->
                     isInGroup = !document.isEmpty
                     bottomNavigation.isClickable = true
@@ -53,12 +55,12 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         if (item.itemId == R.id.ic_movie){
-            replaceFragment(MovieFragment())
+            replaceFragment(movieFragment)
         } else if (item.itemId == R.id.ic_match){
             if(isInGroup){
-                replaceFragment(secondMatchFragment)
-            } else {
                 replaceFragment(matchFragment)
+            } else {
+                replaceFragment(createGroupFragment)
             }
         } else if (item.itemId == R.id.ic_search){
             replaceFragment(searchFragment)
@@ -71,7 +73,25 @@ class MainActivity : AppCompatActivity() {
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fl_wrapper, fragment)
+            addToBackStack(null)
             commit()
+        }
+    }
+
+    override fun onBackPressed() {
+        var fm: FragmentManager = supportFragmentManager
+        when {
+            fm.backStackEntryCount > 0 -> {
+                Log.i(TAG, "popping back stack")
+                fm.popBackStack()
+            }
+            fm.backStackEntryCount == 0 -> {
+                Log.i(TAG, "Nothing on back stack, closing app")
+                exitProcess(-1)
+            }
+            else -> {
+                super.onBackPressed()
+            }
         }
     }
 }
